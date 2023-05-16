1ace537b17b6fa083d5dd1af0a872c760814c177
jak2k <76660118+Jak2k@users.noreply.github.com>
2023-05-16 14:17:02 +0200

    fix: :ambulance: fix error with base-path

--- games/sliding-puzzle/vite.config.ts
+++ games/sliding-puzzle/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { UserConfigExport, defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const config: UserConfigExport = {
+  plugins: [react()],
+};
+
+// in production mode, the base path is /sliding-puzzle/
+
+if (process.env.NODE_ENV === "production") {
+  config.base = "/sliding-puzzle/";
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(config);
